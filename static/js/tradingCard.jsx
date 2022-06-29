4175b43250a99e8fed050bf55ab67e034410dab6
Jessica-Lynn3 <jesslynn19338@gmail.com>
2022-06-28 23:08:40 -0700

    In tradingCard.jsx -- Added comments

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -8,16 +8,23 @@ const tradingCardData = [
   },
 ];
 
+// There is only 1 TradingCard definition because it's using templating to pass in the values for each card
+// Each card is an instance of TradingCard
+// The props for each card are: name, imgUrl, skill
+// We can get the value of a prop at props.nameOfProp (like how they are used in the TradingCard React component below)
 function TradingCard(props) {
   return (
     <div className="card">
       <h2>Name: {props.name}</h2>
       <img src={props.imgUrl} alt="profile" />
-      <h2>Skill: </h2>
+      <h2>Skill: {props.skill} </h2>
     </div>
   );
 }
 
+// By using ReactDOM.render() each trading card is mounted into the DOM (see code below)
+// document.querySelector is used to select elements for each card by id
+// The props are being rendered onto the page by being passed in through the TradingCard React component
 ReactDOM.render(
   <TradingCard name="Balloonicorn" skill="video games" imgUrl="/static/img/balloonicorn.jpg" />,
   document.querySelector('#balloonicorn'),
